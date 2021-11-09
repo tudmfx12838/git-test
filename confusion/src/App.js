ade30108c5cd0fd6b8d115b72461dc2aa8bec2f8
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-11-09 22:45:41 +0900

    Introduction to Redux, configure React-Redux

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -4,18 +4,25 @@ import './App.css';
 import { Component } from 'react/cjs/react.production.min';
 import { BrowserRouter } from 'react-router-dom';
 
-// function App() {
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
+
+// function App() {
 class App extends Component {
   
 
   render(){
     return (
-      <BrowserRouter>
-        <div>
-            <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+              <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+
     );
   }
 }
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -6,32 +6,33 @@ import Contact from './ContactComponet';
 import Footer from './FooterComponent';
 import Menu  from './MenuComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from './shared/dishes';
-import { COMMENTS } from './shared/comments';
-import { LEADERS } from './shared/leaders';
-import { PROMOTIONS } from './shared/promotions';
-import { Switch, Route, Redirect } from 'react-router-dom';
 
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+//withRouter cau hinh ket noi React component voi Redux
+import { connect } from 'react-redux';
+
+//get state and returd state as parameter
+const mapStateToProps = state => {
+  return{
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
 class Main extends Component {
   
   constructor(props){
     super(props);
 
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      leaders: LEADERS,
-      promotions: PROMOTIONS
- 
-    }
   }
 
   render(){
     const HomePage = () => {
       return(
-        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
       )
     }
@@ -39,8 +40,8 @@ class Main extends Component {
     // match is react-router's default param
     const DishWithId = ({match}) => {
       return(
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
         />
       );
     }
@@ -50,8 +51,8 @@ class Main extends Component {
           <Header />
           <Switch>
             <Route path="/home" component={HomePage} />
-            <Route path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
-            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+            <Route path="/aboutus" component={() => <About leaders={this.props.leaders}/>} />
+            <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/>} />
             <Route path="/menu/:dishId" component={DishWithId}/>
             {/* <Route exact path="/contactus" component={() => <Contact/>}/> */}
             <Route exact path="/contactus" component={Contact}/>
@@ -62,5 +63,6 @@ class Main extends Component {
     );
   }
 }
+//cach ket component to react-router
+export default withRouter(connect(mapStateToProps)(Main));
 
-export default Main;

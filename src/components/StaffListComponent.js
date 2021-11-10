6152a07938653012d34c52ed7b13f29c787fc19c
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-11-11 00:31:03 +0900

    2nd commit added Uncontrolled Form

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import dateFormat from 'dateformat';
-import Select from 'react-select';
-import { Card, CardText, CardBody, CardTitle, CardImg, Breadcrumb, BreadcrumbItem, Input } from 'reactstrap';
+// import Select from 'react-select';
+import { Card, CardText, CardBody, CardTitle, CardImg, Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Input, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
+class SearchStaff extends Component{
+    constructor(props){
+        super(props);
+        this.handleSearch = this.handleSearch.bind(this);
+
+    }
+
+    handleSearch(event){
+        alert("value: " + this.search.value);
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <Form onSubmit={this.handleSearch}>
+                <FormGroup row>
+                    <Col md={10}>
+                        <Input type="text" id="search" name="search"
+                            innerRef={(input) => this.search = input} placeholder="Nhập tên nhân viên cần tìm"/>
+                    </Col>
+                    <Col md={2}>
+                        <Button  type="submit" value="submit" color="primary">
+                            Tìm
+                        </Button>
+                    </Col>
+            </FormGroup>
+            </Form>
+        );
+    }
+}
+
 
 function RenderStaff({staff}){
     return(
@@ -14,6 +46,7 @@ function RenderStaff({staff}){
         </Card>
     );
 } 
+
 
 //function StaffList(props)/({staffs})
 var temp = {};
@@ -29,14 +62,25 @@ const StaffList = ({staffs}) => {
     return(
         <div className="container">
             <div className="row">
-                <div className="col-12">
+                <div className="col-6 col-md-3">
                     <h3>Nhân Viên</h3>
-                    <hr />
+                </div>
+                <div className="col-6 col-md-3 mt-1">
+                    <Col>
+                        <Button type="button">
+                            <span  className="fa fa-plus-square"></span>
+                        </Button>
+                    </Col>
+                </div>
+                <div className="col-12 col-md-6 mt-1">
+                    <SearchStaff/>
                 </div>
             </div>
+            <hr />
             <div className="row">
                     {staff}
             </div>
+
         </div>
     );
 }
